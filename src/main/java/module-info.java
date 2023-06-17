3de32e2de7c717013fc5ac710bed45053c967f1e
horita <rikuya2533@gmail.com>
2023-06-17 13:30:38 +0900

    変更

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,7 @@
 module com.example.testapp {
     requires javafx.controls;
     requires javafx.fxml;
-
     requires org.kordamp.bootstrapfx.core;
-    requires com.google.auth.oauth2;
-    requires firebase.admin;
-
     opens com.example.testapp to javafx.fxml;
     exports com.example.testapp;
 }
