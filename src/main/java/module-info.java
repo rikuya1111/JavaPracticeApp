9c5982547ba3887617c692c030f7e9fb52a037e7
rikuya1111 <134114236+rikuya1111@users.noreply.github.com>
2023-06-17 17:02:54 +0900

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.example.testapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires org.kordamp.bootstrapfx.core;
-    opens com.example.testapp to javafx.fxml;
-    exports com.example.testapp;
-}
